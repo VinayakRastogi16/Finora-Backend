b47dd2914313fff8d6660367f8129acfb2a7fea2
Vinayak Rastogi <vinayakr841@gmail.com>
2026-03-01 15:46:17 +0530

    /

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ mongoose.connect(uri)
 
 const allowedOrigins = [
  "https://finora-y3fy.netlify.app",
-  "https://finora-dashboard-y3fy.netlify.app/",
+  "https://finora-dashboard-y3fy.netlify.app",
 ];
 
 app.use(cors({
